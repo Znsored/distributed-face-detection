70926da25eff57893cdb6c513bf5a0bb74413886
Athreya99 <athreya.manoj99@gmail.com>
2023-06-01 19:56:40 -0700

    replace server.py

--- server.py
+++ server.py
@@ -1,74 +1,114 @@
+import time
 import zmq
-import numpy as np
 import cv2
+import numpy as np
+import os
 import json
 import base64
-from faceDetector import get_features
-import time
+from flask import Flask, request
 
-def main():
-    # Worker configuration
-    server_address = "tcp://127.0.0.1:5554"
-    worker_address = "tcp://127.0.0.1:5556"
+app = Flask(__name__)
+sa
+def process_video(video_path):
+    # Server configuration
+    server_address = ["tcp://*:5554", "tcp://*:5555"]
+    worker_addresses = ["tcp://*:5556", "tcp://*:5557"]
 
     context = zmq.Context()
 
-    # Create a PULL socket for receiving jobs from the server
-    pull_socket = context.socket(zmq.PULL)
-    pull_socket.connect(server_address)
+    print("Server started. Pushing jobs to workers...")
 
-    # Create a PUSH socket for sending acknowledgments to the server
-    push_socket = context.socket(zmq.PUSH)
-    push_socket.connect(worker_address)
+    video = cv2.VideoCapture(video_path)
+    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    job_count = 11
 
-    while True:
-        # Receive the job (frame ID and image) from the server
-        job_json = pull_socket.recv()
-        job_data = json.loads(job_json.decode('utf-8'))
+    # Create the directory to store frames
+    frames_dir = '/tmp/frames'
+    os.makedirs(frames_dir, exist_ok=True)
 
-        # Extract frame ID and image from the received JSON
-        frame_id = job_data['frame_id']
-        image_base64 = job_data['image']
+    while job_count <= frame_count+10:
+        # print(f"Job count: {job_count}")
+        # print(f"Frame count: {frame_count+10}")
 
-        # Decode the image from Base64
-        image_data = base64.b64decode(image_base64)
+        # Get the next worker address in a cyclic manner
+        worker_address = worker_addresses[job_count % len(worker_addresses)]
+        server_address_push = server_address[job_count % len(worker_addresses)]
 
-        # Convert the image data to NumPy array
-        image_array = np.frombuffer(image_data, dtype=np.uint8)
+        push_socket = context.socket(zmq.PUSH)
+        push_socket.bind(server_address_push)
 
-        # Decode the image array using OpenCV
-        # image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        ret, frame = video.read()
+        if not ret:
+            break
 
-        # Process the image
-        start_time = time.time()
-        processed_image = get_features(image_array)
-        end_time = time.time()
-        time_taken = end_time - start_time
-        print(time_taken)
+        # Serialize the frame
+        frame_data = cv2.imencode('.jpg', frame)[1].tobytes()
 
-        # Convert the processed image to grayscale
-        # processed_image_gray = cv2.cvtColor(processed_image, cv2.COLOR_BGR2GRAY)
-        #
-        # # Encode the processed image5
-        # _, processed_image_data = cv2.imencode('.jpg', processed_image)
-        processed_image_base64 = base64.b64encode(processed_image).decode('utf-8')
+        # Convert the frame data to base64
+        frame_base64 = base64.b64encode(frame_data).decode('utf-8')
 
-        # Prepare the acknowledgment data in JSON format
-        ack_data = {
-            'frame_id': frame_id,
-            'time_taken': time_taken,
-            'worker_id': 1,
-            'image': processed_image_base64
+        # Prepare the job data in JSON format
+        job_data = {
+            'frame_id': job_count,
+            'image': frame_base64
         }
-        ack_json = json.dumps(ack_data)
+        job_json = json.dumps(job_data)
 
-        # Send the acknowledgment JSON back to the server
-        push_socket.send(ack_json.encode())
+        # Send the job data to the worker
+        push_socket.send(job_json.encode())
+
+        print(f"Sent frame {job_count} to worker {worker_address}")
+
+        # Create a PULL socket to receive the acknowledgment from the worker
+        pull_socket = context.socket(zmq.PULL)
+        pull_socket.bind(worker_address)
+        ack_json = pull_socket.recv()
+
+        # Deserialize the received acknowledgment JSON
+        ack_data = json.loads(ack_json.decode())
+
+        # Extract acknowledgment data
+        frame_id = ack_data['frame_id']
+        time_taken=ack_data['time_taken']
+        worker_id=ack_data['worker_id']
+        processed_image_base64 = ack_data['image']
+        print(f"{frame_id}  processed by {worker_id} in {time_taken}")
+        # Convert the processed image from base64 to NumPy array
+        processed_image_data = base64.b64decode(processed_image_base64)
+        processed_image_array = np.frombuffer(processed_image_data, dtype=np.uint8)
+
+        # Decode the processed image array using OpenCV
+        processed_image = cv2.imdecode(processed_image_array, cv2.IMREAD_COLOR)
+
+        # Save the processed image
+        frame_path = os.path.join(frames_dir, f"frame_{frame_id}.jpg")
+        cv2.imwrite(frame_path, processed_image)
+        # print(f"Saved frame {frame_id} at {frame_path}")
+
+        job_count += 1
+        if job_count >= frame_count+10:
+            print("All frames processed")
+            # push_socket.close()
+            # context.term()
+            # video.release()
+            # return 
 
     # Close the sockets and terminate the context
-    pull_socket.close()
-    push_socket.close()
-    context.term()
+
+
+@app.route('/process_video', methods=['POST'])
+def process_video_route():
+    if 'video' not in request.files:
+        return "No video file provided", 400
+
+    video_file = request.files['video']
+    video_path = 'uploaded_video.mp4'
+    video_file.save(video_path)
+
+    # Process the video
+    process_video(video_path)
+    return "Video processed successfully"
+
 
 if __name__ == "__main__":
-    main()
+    app.run(port=5000)
